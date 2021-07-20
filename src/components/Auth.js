47ea0326c8c36f5e447e3265d35cdc21c80152cf
zvenko <lucianmg05@gmail.com>
2021-07-20 16:45:23 +0300

    split the store into multiple files

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,19 +1,21 @@
 import { useDispatch } from 'react-redux';
 
 import classes from './Auth.module.css';
-import { authActions } from '../store/index';
+import { authActions } from '../store/auth';
 
 const Auth = () => {
   const dispatch = useDispatch();
 
-  const loginHandler = () => {
+  const loginHandler = (event) => {
+    event.preventDefault();
+
     dispatch(authActions.login());
   };
 
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
@@ -22,7 +24,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button onClick={loginHandler}>Login</button>
+          <button type='submit'>Login</button>
         </form>
       </section>
     </main>
